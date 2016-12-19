89604b14b58c831fc2916e0322cda1190a6e1bff
biswajeet619 <biswajeet15696@gmail.com>
2016-12-20 00:37:37 +0530

    HomePage

---
+++ app/src/main/java/com/example/biswajeet/anewbeginning/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package com.example.biswajeet.anewbeginning;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, HomeActivity.class)); //moves from splash screen to home activity
+                SplashScreenActivity.this.finish();     //CLOSES THE activity after returning back(app closes)
+            }
+        }, 3000);   //displays the splash screen for 300ms (3Seconds)
+    }
+}
